f219a79915455cf78d13bd5ff1b959c926e542f1
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-16 16:01:29 -0500

    Jest config for sc components

--- packages/cs-components/.storybook/main.js
+++ packages/cs-components/.storybook/main.js
@@ -1,7 +1,7 @@
 const custom = require('../webpack.config.js')
 
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../components**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5'
